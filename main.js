5f79d50f1c9de5c8bd06e1a7100597af112da625
khayrullaevna <khayrullaevna.g@gmail.com>
2023-07-22 12:52:01 +0500

    change

---
+++ main.js
@@ -0,0 +1,26 @@
+// 1
+var a1 = 10 
+var b1 = 5
+console.log(a1+b1)
+
+// 2
+let a2 = 25;
+let b2 = 15;
+let result2 = a2 - b2;
+console.log(result2)
+
+// 3
+let a3 = 10;
+let b3 = 5;
+let c3 = a3 - b3;
+console.log(c3);
+let d = 7;
+let result3 = c3 * d
+console.log(result3)
+
+// 4
+let a4 = 10;
+let b4 = 3;
+console.log(a4 % b4)
+
+console.log(Math.pow(2, 10))
